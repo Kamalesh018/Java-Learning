373ef6a84fda8e1e4f00a1a00feaf750b2007cd7
Kamalesh018 <kamaleshkb77@gmail.com>
2023-04-18 20:50:58 +0530

    interface learning

--- Interface/src/kamalesh/abs/Animal.java
+++ Interface/src/kamalesh/abs/Animal.java
@@ -1,4 +1,56 @@
 package kamalesh.abs;
+
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE,DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if(this != GROUNDED) {
+            System.out.println("Monitoring " + this);
+        }
+    }
+}
+record DragonFLy(String name, String type) implements FlightEnbedded {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+class Statellite implements OrbitEarth {
+     public void achieveOrbit() {
+        System.out.println("Orbit achieved");
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnbedded {
+    void achieveOrbit();
+}
 interface FlightEnbedded {
     double MILES_TO_KM = 1.60934;
     double KM_TO_MILES = 0.621371;
--- Interface/src/kamalesh/abs/Main.java
+++ Interface/src/kamalesh/abs/Main.java
@@ -1,6 +1,9 @@
 package kamalesh.abs;
 
 import javax.sound.midi.Track;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,6 +30,20 @@ public class Main {
         double kmsTraveled = 100;
         double milesTraveled = kmsTraveled * FlightEnbedded.KM_TO_MILES;
         System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmsTraveled,milesTraveled);
+
+        LinkedList<FlightEnbedded> fliers = new LinkedList<>();
+        fliers.add(bird);
+
+        List<FlightEnbedded> betterFliers = new LinkedList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
 
     private static void inFlight (FlightEnbedded flier) {
@@ -36,5 +53,21 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
+    private static void triggerFliers(List<FlightEnbedded> fliers){
+        for (var flier : fliers) {
+            flier.takeOff();
+        }
+    }
+
+    private static void flyFliers(List<FlightEnbedded> fliers){
+        for (var flier : fliers) {
+            flier.fly();
+        }
+    }
+    private static void landFliers(List<FlightEnbedded> fliers){
+        for (var flier : fliers) {
+            flier.land();
+        }
     }
 }
